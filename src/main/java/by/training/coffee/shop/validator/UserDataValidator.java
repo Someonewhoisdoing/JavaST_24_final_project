bd13f4336aa0246c9f9b80bba5f5f0e93055d2b1
Olga Ivanova <someonewhoisdoing@mail.ru>
2020-05-15 23:34:42 +0300

    add user data validator

---
+++ src/main/java/by/training/coffee/shop/validator/UserDataValidator.java
@@ -0,0 +1,42 @@
+package by.training.coffee.shop.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserDataValidator {
+    private static final String LOGIN_PATTERN = "([a-zA-Z0-9_]+){6,}";
+    private static final String PASSWORD_PATTERN = "([a-zA-Z0-9_]+){4,}";
+    private static final String NAME_PATTERN = "[A-Za-zА-Яа-я]+";
+    private static final String PHONE_PATTERN = "\\+[0-9]{12}";
+
+
+    public boolean checkData(String login, String password, String name, String surname, String phone) {
+        if (isRegexMatches(login, LOGIN_PATTERN)) {
+            return true;
+        }
+        if (isRegexMatches(password, PASSWORD_PATTERN)) {
+            return true;
+        }
+        if (isRegexMatches(name, NAME_PATTERN)) {
+            return true;
+        }
+        if (isRegexMatches(surname, NAME_PATTERN)) {
+            return true;
+        }
+        return isRegexMatches(phone, PHONE_PATTERN);
+    }
+
+    private boolean isRegexMatches(String data, String regex) {
+        if (data == null) {
+            return false;
+        }
+        return isPatternMatches(data, regex);
+    }
+
+    private boolean isPatternMatches(String data, String currentPattern) {
+        Pattern pattern = Pattern.compile(currentPattern);
+        Matcher matcher = pattern.matcher(data);
+
+        return matcher.matches();
+    }
+}
