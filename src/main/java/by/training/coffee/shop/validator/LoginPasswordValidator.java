a2dee4b15e8f61b3dadbe5e73db0fd4586cd517c
Olga Ivanova <someonewhoisdoing@mail.ru>
2020-05-15 23:47:17 +0300

    add login and password data validator

---
+++ src/main/java/by/training/coffee/shop/validator/LoginPasswordValidator.java
@@ -0,0 +1,30 @@
+package by.training.coffee.shop.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LoginPasswordValidator {
+    private static final String LOGIN_PATTERN = "([a-zA-Z0-9_]+){6,}";
+    private static final String PASSWORD_PATTERN = "([a-zA-Z0-9_]+){4,}";
+
+    public boolean checkData(String login, String password) {
+        if (isRegexMatches(login, LOGIN_PATTERN)) {
+            return true;
+        }
+        return isRegexMatches(password, PASSWORD_PATTERN);
+    }
+
+    private boolean isRegexMatches(String data, String regex) {
+        if (data == null) {
+            return false;
+        }
+        return isPatternMatches(data, regex);
+    }
+
+    private boolean isPatternMatches(String data, String currentPattern) {
+        Pattern pattern = Pattern.compile(currentPattern);
+        Matcher matcher = pattern.matcher(data);
+
+        return matcher.matches();
+    }
+}
