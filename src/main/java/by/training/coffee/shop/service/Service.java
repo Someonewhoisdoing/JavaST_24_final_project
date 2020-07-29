416b8940f317932421f2956941f3aff02835cbd7
Olga Ivanova <someonewhoisdoing@mail.ru>
2020-07-29 23:17:26 +0300

    rename the class

--- src/main/java/by/training/coffee/shop/service/Service.java
+++ src/main/java/by/training/coffee/shop/service/Service.java
@@ -4,7 +4,7 @@ import by.training.coffee.shop.entity.Entity;
 import by.training.coffee.shop.exception.DAOException;
 import by.training.coffee.shop.exception.ServiceException;
 
-public interface ServiceForDAO<T extends Entity> {
+public interface Service<T extends Entity> {
 
     boolean create(T entity) throws DAOException, ServiceException;
 }
