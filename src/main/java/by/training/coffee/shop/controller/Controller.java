4a0754b5f628778ed0374c15e7bbb3bc17de4a60
Olga Ivanova <someonewhoisdoing@mail.ru>
2020-07-29 23:23:57 +0300

    change process method

--- src/main/java/by/training/coffee/shop/controller/Controller.java
+++ src/main/java/by/training/coffee/shop/controller/Controller.java
@@ -1,9 +1,9 @@
 package by.training.coffee.shop.controller;
 
 import by.training.coffee.shop.command.Command;
+import by.training.coffee.shop.command.CommandFactory;
 import by.training.coffee.shop.command.Page;
-import by.training.coffee.shop.command.factory.CommandFactory;
-import by.training.coffee.shop.pool.JDBCConnection;
+import by.training.coffee.shop.exception.ServiceException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -15,71 +15,43 @@ import java.io.IOException;
 
 public class Controller extends HttpServlet {
     private final static Logger logger = LogManager.getLogger(Controller.class);
-
-    public Controller() {
-        //empty constructor
-    }
-
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
-        try {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
             process(request, response);
             logger.info("process in get method");
-        } catch (IOException | ServletException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) {
-        try {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException {
             process(request, response);
             logger.info("process in post method");
-        } catch (IOException | ServletException e) {
-            e.printStackTrace();
-        }
     }
 
-    @Override
-    public void destroy() {
-        super.destroy();
+    private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        String commandValue = request.getParameter("command");
+        Command command = CommandFactory.getCommand(commandValue);
 
-        JDBCConnection jdbcConnection = JDBCConnection.getInstance();
-        jdbcConnection.closeConnection();
-
-        logger.info("connection closed");
-    }
-
-    private void process(HttpServletRequest request, HttpServletResponse response)
-            throws IOException, ServletException {
-        Page page;
-
-        CommandFactory commandFactory = new CommandFactory();
-        Command command = commandFactory.getCommand(request);
-
-        page = command.execute(request, response);
         logger.info("page received in Controller");
-
-        boolean isRedirect = page.isRedirect();
-        if (isRedirect) {
-            redirect(page, request, response);
-        } else {
-            forward(page, request, response);
+        try {
+            Page page = command.execute(request, response);
+            if (page.isRedirect()) {
+                redirect(page, request, response);
+            } else {
+                forward(page, request, response);
+            }
+        } catch (ServiceException | IOException e) {
+            throw new ServletException(e.getMessage(),e);
         }
     }
 
-    private void redirect(Page page, HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
+    private void redirect(Page page, HttpServletRequest request, HttpServletResponse response) throws IOException {
         String url = page.getPageUrl();
-
         response.sendRedirect(request.getContextPath() + url);
         logger.info("response redirected");
     }
 
-    private void forward(Page page, HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    private void forward(Page page, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String url = page.getPageUrl();
-
         request.getRequestDispatcher(url).forward(request, response);
         logger.info("request forwarded");
     }
